6e228db76823d6e3ed93550c1ef88b2d2a9b26ff
lamnaourm <mohammed.lamnaour@gmail.com>
2022-11-05 13:36:33 +0100

    stream

--- src/Principale.java
+++ src/Principale.java
@@ -1,18 +1,44 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import com.ismo.dao.DAOProduit;
 import com.ismo.dao.IDao;
 import com.ismo.model.Produit;
 
 public class Principale {
+	
+	public static void displayProds(List<Produit> p) {
+		
+		p.stream().forEach(item -> System.out.println(item));
+	}
 
 	public static void main(String[] args) throws FileNotFoundException, ClassNotFoundException, IOException, SQLException {
 		
 		IDao<Produit> dao = new DAOProduit();
 		
-		dao.delete(6);
+		List<Produit> prds = dao.getAll();
+		
+		displayProds(prds);
+		
+		System.out.println("--------------------------------------------------");
+		List<Produit> qst2 = prds.stream()
+				.filter(item -> item.getNom().startsWith("d") && item.getPrix_achat() > 20)
+				.collect(Collectors.toList());
+		
+		displayProds(qst2);
+		System.out.println("--------------------------------------------------");
+		List<Produit> qst3 = prds.stream()
+				.map(item -> {
+					item.setPrix_achat(item.getPrix_achat()*1.2);
+					return item;
+				})
+				.collect(Collectors.toList());
+		
+		displayProds(qst3);
 
 	}
 
